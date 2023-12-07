ffa1207c36e2f9c675d6f674b7e93abd7bc8d0f2
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2023-12-07 12:56:53 +0800

    v2

--- k.sh
+++ k.sh
@@ -1,5 +1,2 @@
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.4.tar.xz
-tar -xvf linux-6.6.4.tar.gz -C ~/
-apt update
 sudo apt install vim build-essential libncurses-dev libssl-dev libelf-dev bison flex -y
-cd linux-6.6.4/
